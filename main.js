db019856a3b3c43212471f424defb1d20e1bd92c
Marta1607 <marta.kar.zuber@gmail.com>
2021-02-26 14:29:45 +0100

    koniec

--- main.js
+++ main.js
@@ -6,5 +6,8 @@ tablica.forEach(item => {
 console.log(tablica.every ( item => item > 1)) //fałsz
 console.log(tablica.indexOf("Jan"))
 
+const liczby = new Array(1, 15, 150, 1500, 5525,)
+console.log(liczby)
 
-
+const imiona = new Array("Jan", "Henryk")
+console.log(imiona)
